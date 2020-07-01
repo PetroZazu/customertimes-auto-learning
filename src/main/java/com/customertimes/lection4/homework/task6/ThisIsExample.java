9133f9fcd55cc1da882025b24d81c3cc13ba0769
Petro Zazuliak <petro.zazuliak@gmail.com>
2020-07-01 23:54:34 +0300

    task6 ready

--- src/main/java/com/customertimes/lection4/homework/task6/ThisIsExample.java
+++ src/main/java/com/customertimes/lection4/homework/task6/ThisIsExample.java
@@ -1,5 +1,24 @@
 package com.customertimes.lection4.homework.task6;
 
 public class ThisIsExample {
+    String field1;
+    int field2;
+    int field3 = 30;
 
-}
+    public ThisIsExample(String field1, int TheParametr2, int field3) {
+        field1 = field1; // allways equal null
+        field2 = TheParametr2; // no need this.
+        this.field3 = field3; // this. give variable from paramtr to the field 3
+    }
+
+    public void printVariables() {
+        System.out.println(field1);
+        System.out.println(field2);
+        System.out.println(field3);
+    }
+        public static void main(String[]args){
+            ThisIsExample example = new ThisIsExample("skr", 1, 2);
+            example.printVariables();
+        }
+    }
+
