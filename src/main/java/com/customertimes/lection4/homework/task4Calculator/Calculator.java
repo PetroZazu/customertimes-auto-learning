f264722a283c65edf15aadc1e962e8560e23b178
Petro Zazuliak <petro.zazuliak@gmail.com>
2020-07-03 13:42:37 +0300

    Calculator changes v2

--- src/main/java/com/customertimes/lection4/homework/task4Calculator/Calculator.java
+++ src/main/java/com/customertimes/lection4/homework/task4Calculator/Calculator.java
@@ -1,9 +1,19 @@
 package com.customertimes.lection4.homework.task4Calculator;
+/*
+4. Create a class Calculator. - done
+ It should have three fields: double firstValue, double secondValue and String operation. - done
+ Create all required constructors to properly initialize your object. -done
+ Consider the case when user does not provide any parameters when creates a new object. -done
+ Write a method "calculate" that takes these two values and return the result of the calculation according to the operation.
+ Write the test class which contains the main method.
+ In this main method you should create your Calculator object and do some calculation to test your code,
+ by calling your "calculate" method. Print all results to the console.
+ */
 
 public class Calculator {
-    double firstValue;
-    double secondValue;
-    String operation;
+    private double firstValue;
+    private double secondValue;
+    private String operation;
 
     public Calculator(double firstValue, String operation, double secondValue) {
         this.firstValue = firstValue;
@@ -11,37 +21,35 @@ public class Calculator {
         this.operation = operation;
     }
 
-    Calculator c;
 
     public Calculator() {
-
+        System.out.println("please provide arguments");
     }
 
-    public static void calc(Calculator c) {
-        if (c.operation.equals("+")) {
-            System.out.println(c.firstValue + c.secondValue);
-            System. exit(1);
+    public void calc() {
+        switch (operation) {
+            case "*":
+                System.out.println(this.firstValue * this.secondValue);
+                return;
+            case "+":
+                System.out.println(this.firstValue + this.secondValue);
+                return;
+            case "-":
+                System.out.println(this.firstValue - this.secondValue);
+                return;
+            case "/":
+                System.out.println(this.firstValue / this.secondValue);
+                return;
+            default:
+                System.out.println("Please enter a valid operation, such as:");
+                System.out.println("+");
+                System.out.println("or");
+                System.out.println("-");
+                System.out.println("or");
+                System.out.println("/");
+                System.out.println("or");
+                System.out.println("*");
         }
-        if (c.operation.equals("-")) {
-            System.out.println(c.firstValue - c.secondValue);
-            System. exit(1);
-        }
-        if (c.operation.equals("/")) {
-            System.out.println(c.firstValue / c.secondValue);
-            System. exit(1);
-        }
-        if (c.operation.equals("*")) {
-            System.out.println(c.firstValue * c.secondValue);
-            System. exit(1);
-        } else {
-            System.out.println("Please enter a valid operation, such as:");
-            System.out.println("+");
-            System.out.println("or");
-            System.out.println("-");
-            System.out.println("or");
-            System.out.println("/");
-            System.out.println("or");
-            System.out.println("*");
-        }
+
     }
 }
--- src/main/java/com/customertimes/lection4/homework/task4Calculator/TestCalc.java
+++ src/main/java/com/customertimes/lection4/homework/task4Calculator/TestCalc.java
@@ -2,7 +2,8 @@ package com.customertimes.lection4.homework.task4Calculator;
 
 public class TestCalc {
     public static void main(String[] args) {
-        Calculator add = new Calculator(33, "*", 3);
-        Calculator.calc(add);
+        Calculator operation = new Calculator(33, "-", 3);
+        operation.calc();
+
     }
 }
