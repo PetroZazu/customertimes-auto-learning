e90df0b0848ea430c616c58d880b6cc1c025c026
Petro Zazuliak <petro.zazuliak@gmail.com>
2020-07-28 11:56:03 +0300

    lesson 8, task 3 fix

--- src/main/java/com/customertimes/lection8/task3/Main.java
+++ src/main/java/com/customertimes/lection8/task3/Main.java
@@ -24,17 +24,15 @@ public class Main {
             int a = args.length;
             System.out.println(a);
             int b = 10 / a;
+            int[] c = {1};
+            System.out.println(c[1]);
 
         } catch (ArithmeticException nPE) {
             System.out.println("you can not divide by 0");
-        }
-
-        try {
-            int[] c = {1};
-            System.out.println(c[1]);
         } catch (IndexOutOfBoundsException iOOBE) {
             System.out.println("Array out of bound exception");
         }
+
 
         System.out.println("code execution after exceptions");
     }
