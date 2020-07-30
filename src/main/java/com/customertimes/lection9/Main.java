7410063c8dfa3f7c93d8f9d1567ea0ad2b8ef858
Petro Zazuliak <petro.zazuliak@gmail.com>
2020-07-30 14:21:23 +0300

    Lesoson 9, task tath updates

--- src/main/java/com/customertimes/lection9/Main.java
+++ src/main/java/com/customertimes/lection9/Main.java
@@ -9,30 +9,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
         final String sp = File.separator;
         final Path srcPath = Paths.get
                 (System.getProperty("user.dir"));
 
-        String inputPath =
+        
+
+        String pathToInFile =
                 srcPath + sp + "src" + sp + "main" + sp + "java" + sp + "com" + sp + "customertimes" + sp + "lection9" + sp + "inputData.txt";
-        String outputPath =
+        String pathToOutFile =
                 srcPath + sp + "src" + sp + "main" + sp + "java" + sp + "com" + sp + "customertimes" + sp + "lection9" + sp + "outputData.txt";
 
 
-        Path pathInput = Paths.get(inputPath);
-        Path pathOutput = Paths.get(outputPath);
+        Path inPath = Paths.get(pathToInFile);
+        Path outPath = Paths.get(pathToOutFile);
 
-        List<String> lines = Files.readAllLines(pathInput);
+
+        List<String> lines = Files.readAllLines(inPath);
         List<String> outputLines = new ArrayList<>();
         for (int i = lines.size() - 1; i >= 0; i--) {
             if (!lines.get(i).endsWith("5")) {
                 outputLines.add(lines.get(i));
             }
         }
-        Files.write(pathOutput, outputLines);
+        Files.write(outPath, outputLines);
 
-        byte[] bytes = Files.readAllBytes(pathOutput);
+        byte[] bytes = Files.readAllBytes(outPath);
         System.out.print(new String(bytes));
 
 
